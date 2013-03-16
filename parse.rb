e9290946da28fd2933e2a3647d2d7eed59c07395
EddM <eddm@me.com>
2013-03-16 15:40:24 +0000

    Don't fail on bad games

--- parse.rb
+++ parse.rb
@@ -41,11 +41,17 @@ class ParseTask
 			if link = table.css('a').select { |a| a.text =~ /Play-by-Play/i }.first
 				key = link.attr('href').split("/")[-1].split(".")[0]
 				unless Game.count(:bbref_key => key) > 0
-					game = Game.new
-					url = "http://www.basketball-reference.com#{link.attr('href')}"
-					puts "Fetching #{url}"
-					game.build_from_html open(url)
-					game.insert_into_db(key)
+					begin
+						game = Game.new
+						url = "http://www.basketball-reference.com#{link.attr('href')}"
+						puts "Fetching #{url}"
+						game.build_from_html open(url)
+						game.insert_into_db(key)
+					rescue => e
+						puts "*** Failed for #{key}"
+						puts e.inspect
+  					puts e.backtrace
+					end
 				end
 			end
 			sleep(3)
